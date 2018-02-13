dcad84f1a5764fd523a28fd2e73ab823c4b8f2f1
Samantha Berk <sberk10@gmail.com>
2018-02-13 11:47:28 -0800

    We replaced the underline space with the correct letter when guessed using .index() and moved user input outside of the class

--- word-guess.rb
+++ word-guess.rb
@@ -1,37 +1,31 @@
 class Game
-  attr_accessor :secret_word, :user_input, :life
+  attr_accessor :secret_word_array, :user_input, :life, :correct_guesses_array
 
   def initialize(secret_word)
     @secret_word_array = secret_word
     @user_input = 3
     @guesses = []
-    @correct_guesses = Array.new(@secret_word_array.length)
+    @correct_guesses_array = Array.new(@secret_word_array.length, "_")
     @life = 5
     puts "#{@secret_word_array}"
-    puts "#{@correct_guesses}"
-    @secret_word_array.each do |space|
-      print "_ "
-    end
   end
 
   # Method to compare user_input to secret_word
-  def compare
-    puts "Please guess a letter:"
-    @user_input = gets.chomp.to_s
-
+  def compare(guess)
+    # puts "Please guess a letter:"
+    # @user_input = gets.chomp.to_s
+    @user_input = guess
 
     @secret_word_array.each do |letter|
       if letter == @user_input
-        print letter
-      else
-        print "_ "
+        @correct_guesses_array[@secret_word_array.index(letter)] = letter
       end
     end
-    if @guesses.include?(@user_input) == false
+    if @correct_guesses_array.include?(@user_input) == false
       @guesses << @user_input
       @life -= 1
       puts "\n False guesses :#{@guesses}"
-    elsif @guesses.include?(@secret_word_array)
+    elsif @correct_guesses_array == @secret_word_array
       @life = 0
       puts "you won!"
     end
@@ -42,13 +36,16 @@ word_list = ["cat", "dog", "horse", "bird"]
 new_game = Game.new(word_list.sample.split(""))
 puts
 
+# get user input
+puts "Welcome to the guessing game"
+puts "Please guess your first letter: "
 
-until new_game.life == 0
-new_game.compare
+until new_game.life == 0 || (new_game.correct_guesses_array == new_game.secret_word_array)
+  guess = gets.chomp.to_s
+  new_game.compare(guess)
+  print new_game.correct_guesses_array
 end
 
-
-# Prompt user for a guess
 
 
 # Compare the guess to the secret_word array
